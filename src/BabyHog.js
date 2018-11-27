4f968def1a64a5f470dbb096dcf55d77ccecd05c
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-27 10:08:43 -0500

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
-import normalBaby from './assets/unadulterated-hoglette.png'
 import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
 
-// is there a way we could associate eye color string values with images? 
-// perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+const imgMapper = {
+  'blue': BlueBaby,
+  'glowing': GlowingBaby,
+  'sun': SunBaby
+}
 
 export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 200
+    }
   }
 
   changeWeight = (e) => {
-    // nothing needs to change here
     const newWeight = e.target.name === "+" ? (this.state.weight + 10) : (this.state.weight - 10)
     this.setState({
       weight: newWeight
@@ -25,20 +29,20 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>{this.props.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {this.props.hobby}</h3>
+        <h4>Eye Color: {this.state.eyeColor}</h4>
           
-        <Button name="+">
+        <Button name="+" onClick={this.changeWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={imgMapper[this.props.eyeColor]} style={{height: `${this.state.weight}px`}} alt="MasterBlasterJrJr" />
         </div>
         
       </li>
